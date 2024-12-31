09cd822f80e3231360388d51f570e6071af6a00c
me@sanj.dev <me@sanj.dev>
2024-12-31 21:03:05 +0000

    updates

---
+++ .devcontainer/install.sh
@@ -0,0 +1,5 @@
+mkdir tmp_tools
+cd tmp_tools
+
+curl -O https://download.java.net/java/early_access/jextract/22/5/openjdk-22-jextract+5-33_linux-x64_bin.tar.gz
+tar xvf openjdk-22-jextract+5-33_linux-x64_bin.tar.gz
--- bin/linux/ffm_jextract/generateJava.sh
+++ bin/linux/ffm_jextract/generateJava.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-jextract --output src/main/java -t com.coralblocks.javatocppandback.ffm_jextract.generated --source src/main/c/com_coralblocks_javatocppandback_ffm_jextract_HelloWorld.h --header-class-name Hello
+jextract src/main/c/com_coralblocks_javatocppandback_ffm_jextract_HelloWorld.h \
+    --output src/main/java -t com.coralblocks.javatocppandback.ffm_jextract.generated \
+     --header-class-name Hello
 
